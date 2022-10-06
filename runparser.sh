29949770da1ef0154bd678bc226e184813ecc9c2
treaklePaige <61995897+treaklePaige@users.noreply.github.com>
2022-10-05 20:18:31 -0500

    Update runparser.sh

--- runparser.sh
+++ runparser.sh
@@ -1,8 +1,20 @@
-files="$(ls ebay_data)"
-args=""
-for file in $files
-do
-    args="${args} ebay_data/${file}"
+JSON_DIR="./ebay_data"
+SCRIPT_PATH="./TransformationJson.py"
+CATEGORY_TB="./Categories.dat"
+ITEMS_TB="./Items.dat"
+BIDS_TB="./Bids.dat"
+Sellers_TB="./Sellers.dat"
+Buyers_TB = "./Buyers.dat"
+rm -f $CATEGORY_TB
+rm -f $ITEMS_TB
+rm -f $BIDS_TB
+rm -f $Sellers_TB
+rm -f $Buyers_TB
+for i in $(ls $JSON_DIR); do
+  python $SCRIPT_PATH $JSON_DIR/$i
 done
-
-python skeleton_parser.py $args
+sort -u $CATEGORY_TB -o $CATEGORY_TB
+sort -u $ITEMS_TB -o $ITEMS_TB
+sort -u $BIDS_TB -o $BIDS_TB
+sort -u $Sellers_TB -o $Sellers_TB
+sort -u $Buyers_TB -o $Buyers_TB
