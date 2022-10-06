7703aad2295d2d55f0a1d1c029a3230ebea7e517
arielrezinn <arielrezin@gmail.com>
2022-10-05 19:53:53 -0500

    updating the create sql tab

--- create.sql
+++ create.sql
@@ -4,8 +4,9 @@ drop table if exists Categories;
 drop table if exists Bids;
 
 create table Items(ItemID            INTEGER        NOT NULL  PRIMARY KEY,
+                   FOREIGN KEY (SellerID) REFERENCES Seller(SellerID) 
                    Name              VARCHAR(300)   NOT NULL,
-                   Description       VARCHAR(300)   NOT NULL,
+                   Description       VARCHAR(500)   NOT NULL,
                    Currently         VARCHAR(300)   NOT NULL,
                    Number_of_Bids    INTEGER        NOT NULL,
                    First_Bid         VARCHAR(300)   NOT NULL,
@@ -13,18 +14,25 @@ create table Items(ItemID            INTEGER        NOT NULL  PRIMARY KEY,
                    Started           TIMESTAMP      NOT NULL,
                    Ends              TIMESTAMP      NOT NULL
                    );
-create table EbayUsers(UserID         VARCHAR(300)   NOT NULL  PRIMARY KEY,
-                      Rating         INTEGER         NOT NULL,
+create table Bidder(BidderID         VARCHAR(300)   NOT NULL  PRIMARY KEY,
+                      BidderRating         INTEGER         NOT NULL,
                       Country        VARCHAR(300),   
                       Location       VARCHAR(300)
+                      
+create table Seller(SellerID         VARCHAR(300)   NOT NULL  PRIMARY KEY,
+                      SellerRating         INTEGER         NOT NULL,
+                      Country        VARCHAR(300), 
+                      Location       VARCHAR(300)  
                     );
-create table Categories(CategoryName VARCHAR(300)   NOT NULL    PRIMARY KEY,
+create table Categories(CategoryName VARCHAR(300)   NOT NULL,
                         ItemID       INTEGER        NOT NULL,
-                        FOREIGN KEY (ItemID) REFERENCES Items(ItemID)
+                        FOREIGN KEY (ItemID) REFERENCES Items(ItemID),
+                        PRIMARY KEY(ItemID, CategoryName)
                     );
-create table Bids(UserID             VARCHAR(300) NOT NULL,
-                  Time               TIMESTAMP    NOT NULL    PRIMARY KEY,
+create table Bids(BidderID             VARCHAR(300) NOT NULL,
+                  Time               TIMESTAMP    NOT NULL,
                   Amount             INTEGER      NOT NULL,
-                  FOREIGN KEY (UserID) REFERENCES EbayUsers(UserID)
+                  FOREIGN KEY (BidderID) REFERENCES Bidder(BidderID)
+                  PRIMARY KEY(BidderID, Time)
                    );
 
